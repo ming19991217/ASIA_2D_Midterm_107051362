1b6f847c936395665e5aeaae0567777e675adecb
ming19991217 <ming19991217@gmail.com>
2020-11-06 17:50:35 +0800

    80%

---
+++ 2D_Midterm_107051362/Assets/Scripts/DoorUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorUI : MonoBehaviour
+{
+    public GameObject E;
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            E.SetActive(true);
+            anim.SetTrigger("Trigger");
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            E.SetActive(false);
+            anim.SetTrigger("Trigger");
+        }
+
+    }
+}
